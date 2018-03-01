b53c838f711310e09ca9e4b7c90197df2a5b1e5a
noxim <aaro.peramaa@gmail.com>
2018-03-01 21:10:10 +0200

    hey now its prettier and I like it more, thats all that counts right?

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,11 @@
-#![deny(warnings)]
-#![deny(missing_docs)]
-
-//! A logger configured via an environment variable which writes to standard
-//! error with nice colored output for log levels.
-//!
-//! ## Example
-//!
-//! ```
-//! extern crate pretty_env_logger;
-//! #[macro_use] extern crate log;
-//!
-//! fn main() {
-//!     pretty_env_logger::init();
-//!
-//!     trace!("a trace example");
-//!     debug!("deboogging");
-//!     info!("such information");
-//!     warn!("o_O");
-//!     error!("boom");
-//! }
-//! ```
-
+#[macro_use]
+extern crate log;
 extern crate ansi_term;
 extern crate env_logger;
-extern crate log;
 extern crate dogstatsd;
 
 use std::fmt;
-use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
+use std::time::SystemTime;
 
 use ansi_term::{Color, Style};
 use env_logger::Builder;
@@ -61,57 +39,19 @@ impl fmt::Display for DogLevel {
     }
 }
 
-static MAX_MODULE_WIDTH: AtomicUsize = ATOMIC_USIZE_INIT;
-
-/// Initializes the global logger with a pretty env logger.
-///
-/// This should be called early in the execution of a Rust program, and the
-/// global logger may only be initialized once. Future initialization attempts
-/// will return an error.
-///
-/// # Panics
-///
-/// This function fails to set the global logger if one has already been set.
 #[inline]
 pub fn init() {
     try_init().unwrap();
 }
 
-/// Initializes the global logger with a pretty env logger.
-///
-/// This should be called early in the execution of a Rust program, and the
-/// global logger may only be initialized once. Future initialization attempts
-/// will return an error.
-///
-/// # Errors
-///
-/// This function fails to set the global logger if one has already been set.
 pub fn try_init() -> Result<(), log::SetLoggerError> {
     try_init_custom_env("RUST_LOG")
 }
 
-/// Initialized the global logger with a pretty env logger, with a custom variable name.
-///
-/// This should be called early in the execution of a Rust program, and the
-/// global logger may only be initialized once. Future initialization attempts
-/// will return an error.
-///
-/// # Panics
-///
-/// This function fails to set the global logger if one has already been set.
 pub fn init_custom_env(environment_variable_name: &str) {
     try_init_custom_env(environment_variable_name).unwrap();
 }
 
-/// Initialized the global logger with a pretty env logger, with a custom variable name.
-///
-/// This should be called early in the execution of a Rust program, and the
-/// global logger may only be initialized once. Future initialization attempts
-/// will return an error.
-///
-/// # Errors
-///
-/// This function fails to set the global logger if one has already been set.
 pub fn try_init_custom_env(environment_variable_name: &str) -> Result<(), log::SetLoggerError> {
     let mut builder = formatted_builder()?;
 
@@ -122,32 +62,33 @@ pub fn try_init_custom_env(environment_variable_name: &str) -> Result<(), log::S
     builder.try_init()
 }
 
-/// Returns a `env_logger::Builder` for further customization.
-///
-/// This method will return a colored and formatted) `env_logger::Builder`
-/// for further customization. Tefer to env_logger::Build crate documentation
-/// for further details and usage.
-///
-/// This should be called early in the execution of a Rust program, and the
-/// global logger may only be initialized once. Future initialization attempts
-/// will return an error.
-///
-/// # Errors
-///
-/// This function fails to set the global logger if one has already been set.
 pub fn formatted_builder() -> Result<Builder, log::SetLoggerError> {
     let mut builder = Builder::new();
 
-    let dog = Client::new(Options::default()).unwrap();
+    let mut opts = Options::default();
+    opts.namespace = "".to_string();
+    let dog = Client::new(opts).unwrap();
+
+    let start = SystemTime::now();
 
     builder.format(move |f, record| {
         use std::io::Write;
+
+        let now = SystemTime::now();
+        let d = match now.duration_since(start) {
+            Ok(d) => d,
+            Err(e) => e.duration(),
+        };
+
+        let secs = d.as_secs() % 60;
+        let mins = d.as_secs() / 60 % 60;
+        let hours = d.as_secs() / 3600;
+        let time = format!("{}:{:02}:{:02}.{:03}",
+            hours, mins, secs, 0
+        );
+        let l = ColorLevel(record.level());
+
         if let Some(module_path) = record.module_path() {
-            let mut max_width = MAX_MODULE_WIDTH.load(Ordering::Relaxed);
-            if max_width < module_path.len() {
-                MAX_MODULE_WIDTH.store(module_path.len(), Ordering::Relaxed);
-                max_width = module_path.len();
-            }
 
             // our dirty datadog hack, maybe we shouldn't do it here
             let tags = vec![
@@ -155,17 +96,40 @@ pub fn formatted_builder() -> Result<Builder, log::SetLoggerError> {
                 format!("module:{}", module_path),
             ];
             dog.event(module_path, &format!("{}", record.args()), tags).unwrap();
+            
 
-            writeln!(f, " {} {} > {}",
-                     ColorLevel(record.level()),
-                     Style::new().bold().paint(format!("{: <width$}", module_path, width=max_width)),
-                     record.args())
+            writeln!(f, "{} {}",
+                    l,
+                    Style::new().bold().paint(format!("[{} {}]",
+                        time,
+                        module_path)));
+            writeln!(f, "{} {}", 
+                l,
+                format!("{}",record.args()).replace("\n", &format!("\n{} ", l)))
         } else {
-            writeln!(f, " {} > {}",
-                     ColorLevel(record.level()),
-                     record.args())
+            writeln!(f, "{} {}",
+                    l,
+                    Style::new().bold().paint(format!("[{}]",
+                        time)));
+            writeln!(f, "{} {}", 
+                l,
+                format!("{}",record.args()).replace("\n", &format!("\n{} ", l)))
         }
     });
 
     Ok(builder)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn log_types() {
+        use init;
+        init();
+        trace!("We are tracing now!");
+        debug!("Debugging works fine");
+        info!("This is an info level message: {}", 3 + 5);
+        warn!("Warnings should be yellowish\nOh and by the way this is multiline");
+        error!("Oofie owie this is serious");
+    }
 }
